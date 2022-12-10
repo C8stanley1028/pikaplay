584777aa8a8db55015322264f9c6e2319c6b279c
raychiu135 <91177751+raychiu135@users.noreply.github.com>
2022-12-10 15:05:47 +0800

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <cstdlib>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_net.h>
+#include <SDL2/SDL_ttf.h>
 
 const int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 const float G = 0.3;
 const int LEFT_MOVE = -4, RIGHT_MOVE = 4;
-int player1_jump_V = -12;
-int player1_pos_X = 200, player1_pos_Y = 600;
-bool player1_isJumping = false;
-int jumptime = 0;
+int player1_jump_V = -12, player2_jump_V = -12;
+int player1_pos_X = 200, player1_pos_Y = 600, player2_pos_X = 600, player2_pos_Y = 600;
+bool player1_isJumping = false, player2_isJumping = false;
+int player1_jumptime = 0, player2_jumptime;
 
-int main()
+void player1_position()
 {
-    if(keystate[SDL_SCANCODE_UP])
+    if(keystate[SDL_SCANCODE_UP] && player1_pos_Y == 600)
     {
         player1_isJumping = true;
     }
-    if(keystate[SDL_SCANCODE_RIGHT])
+    if(keystate[SDL_SCANCODE_RIGHT] && player1_pos_x < 400)
     {
         player1_pos_X += RIGHT_MOVE;
     }
@@ -27,16 +32,64 @@ int main()
     if(player1_isJumping)
     {
         player1_pos_Y += player1_jump_V;
-        player1_jump_V -= G * jumptime;
-        jumptime += 1;
+        player1_jump_V -= G * player1_jumptime;
+        player1_jumptime += 1;
         
         if(jumptime == 80)
         {
             player1_pos_Y = 600;
             player1_jump_V = -12;
-            jumptime = 0;
+            player1_jumptime = 0;
         }
     }
+}
+
+void player2_position()
+{
+    if(keystate[SDL_SCANCODE_UP] && player2_pos_Y == 600)
+    {
+        player2_isJumping = true;
+    }
+    if(keystate[SDL_SCANCODE_RIGHT])
+    {
+        player2_pos_X += RIGHT_MOVE;
+    }
+    if(keystate[SDL_SCANCODE_LEFT] && player2_pos_x > 400)
+    {
+        player2_pos_X += LEFT_MOVE;
+    }
     
-    return 0;
+    if(player2_isJumping)
+    {
+        player2_pos_Y += player2_jump_V;
+        player2_jump_V -= G * player2_jumptime;
+        player2_jumptime += 1;
+        
+        if(jumptime == 80)
+        {
+            player2_pos_Y = 600;
+            player2_jump_V = -12;
+            player2_jumptime = 0;
+        }
+    }
+}
+
+int main()
+{
+	while(true)
+	{
+		player1_position();
+		player2_position();
+		
+		cout << "(" << player1_pos_X << "," << player1_pos_Y << ")" << "\n";
+		cout << "(" << player2_pos_X << "," << player2_pos_Y << ")" << "\n";
+		
+		
+		if(keystate[SDL_SCANCODE_BACKSPACE])
+			break;
+	}
+	
+	cout << "END";
+	
+	return 0;
 }
